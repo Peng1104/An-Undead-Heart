2851078ea749aebca077baafabb650e1043bc1c1
Peng1104 <lucas.hix@hotmail.com>
2019-05-10 10:42:58 -0300

    Terminado Sistema de Captura de Imagens

--- DefaultClasses.py
+++ DefaultClasses.py
@@ -1,32 +1,37 @@
 import os
 import pygame
+from pathlib import Path
 
-class Maps():
+class LoadImagens():
 
-	def __init__(self):
-		self.maps = self.loadFile("Configurações do Jogo/Imagens/Mapas", {})
+	def __init__(self, FilePath):
+		if type(FilePath) == str:
+			if Path(FilePath).is_file():
+				raise NotADirectoryError("FilePath não é um Dirétorio!!")
+			else:
+				self.maps = self.loadFile(FilePath, {})
+		else:
+			raise TypeError("ERRO! FilePath deve ser uma String")
 
-	def loadFile(self, path, maps):
-		for location, d, lista in os.walk(path):
+	def __loadFile__(self, FilePath, maps):
+		for location, d, lista in os.walk(FilePath):
 			for file in lista:
 				if file.find(".") > 0:
-					if location[location.rfind("/")+1:] in maps:
-						lista2 = maps[location[location.rfind("/")+1:]]
+					if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".gif") or file.endswith(".bmp") or file.endswith(".pcx") or file.endswith(".tga") or file.endswith(".tif") or file.endswith(".lbm") orfile.endswith(".pbm") or orfile.endswith(".pgm") or orfile.endswith(".ppm") or orfile.endswith(".xpm"):  
+						if file[:-4] in maps:
+							continue
+						else:
+							imagem = pygame.image.load(location + "/" + file)
+							Largura = imagem.get_size()[0]
+							Altura = imagem.get_size()[1]
+							Centro = (Largura/2, Altura/2)
 
-						lista2.append(pygame.image.load(location + "/" + file))
-
-						maps[location[location.rfind("/")+1:]] = lista2
+							maps[file[:-4]] = [imagem, Largura, Altura, Centro]
 					else:
-						maps[location[location.rfind("/")+1:]] = [pygame.image.load(location + "/" + file)]
+						continue
 				else:
-					maps[location[location.rfind("/")+1:]] = self.loadFile(location + "/" + file, {})
+					maps[file] = self.loadFile(location + "/" + file, {})
 		return maps
 
-class DefaultPlayer(pygame.sprite.Sprite):
-
-	def __init__(self):
-		super().__init__()
-
-		self.PlayerImage = None
-
-		self.rect = self.image.get_rect()
+		def getImages(self):
+			return self.maps
